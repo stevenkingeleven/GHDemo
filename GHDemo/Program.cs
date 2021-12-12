ddefc5be2c41c95f967453ee7dbcf19b3ec675b0
ayuqingyong@hotmail.com <ayuqingyong@hotmail.com>
2021-12-13 00:51:57 +0900

    zzzz

--- GHDemo/Program.cs
+++ GHDemo/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("From X3700");
 Console.WriteLine("Hello, World!");
+Console.WriteLine("From 95");
+
 Console.WriteLine("From X3700");
 Console.WriteLine("From X3700");
 Console.WriteLine("From X3700");
